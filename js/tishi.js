4c5c0d67545c65a47441567bcf5d14b7e981738a
洲总 <672110619@qq.com>
2017-12-09 19:55:25 +0800

    no message

---
+++ js/tishi.js
@@ -0,0 +1,12 @@
+  $(document).ready(function () {
+    $('.tooltipped').tooltip();
+  });
+})(jQuery);
+; /*!
+  * Waves v0.6.4
+  * http://fian.my.id/Waves
+  *
+  * Copyright 2014 Alfiana E. Sibuea and other contributors
+  * Released under the MIT license
+  * https://github.com/fians/Waves/blob/master/LICENSE
+  */
